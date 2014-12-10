c21cb1a246a91e462b5234047e7dad7b749ffbeb
Trevor Stone <TrevorSStone@gmail.com>
2014-12-10 01:04:22 -0500

    Start of Account Balance Structs

--- accounts.go
+++ accounts.go
@@ -16,6 +16,47 @@ type Account struct {
 	RegistrationType string
 }
 
+type AccountBalance struct {
+	AccountID         int
+	AccountType       string
+	OptionLevel       string
+	BasicBalance      AccountBasicBalance
+	MarginBalance     AccountMarginBalance
+	DayTradingBalance AccountDayTradingBalance
+	CashBalance       AccountCashBalance
+}
+
+type AccountBasicBalance struct {
+	CashAvailableForWithdrawal  IntDollar
+	CashCall                    IntDollar
+	FundsWithheldFromWithdrawal IntDollar
+	NetAccountValue             IntDollar
+	NetCash                     IntDollar
+	SweepDepositAmount          IntDollar
+	TotalLongValue              IntDollar
+	TotalSecuritiesMktValue     IntDollar
+	TotalCash                   IntDollar
+}
+type AccountMarginBalance struct {
+	FedCall                           IntDollar
+	MarginBalance                     IntDollar
+	MarginBalanceWithdrawal           IntDollar
+	MarginEquity                      IntDollar
+	MarginEquityPercent               float32
+	MarginableSecurities              IntDollar
+	MaxAvailableForWithdrawal         IntDollar
+	MinEquityCall                     IntDollar
+	NonMarginableSecuritiesAndOptions IntDollar
+	TotalShortValue                   IntDollar
+	ShortReserve                      IntDollar
+}
+
+type AccountDayTradingBalance struct {
+}
+
+type AccountCashBalance struct {
+}
+
 const (
 	accountsUrlPath       = "accounts"
 	accountListUrlPath    = "accountlist"
@@ -52,21 +93,9 @@ func (rawAccountListResponse accountListResponse) convert() (accounts []Account,
 	rawList := rawAccountListResponse.AccountListResponse.Accounts
 	accounts = make([]Account, len(rawList), len(rawList))
 	for i, r := range rawList {
-		var netValue IntDollar
-		switch v := r.NetAccountValue.(type) {
-		default:
-			return accounts, errors.New("Unexpected type in response for Value")
-		case float32:
-			netValue = IntDollar(math.Floor((float64(v) * 100) + .5))
-		case string:
-			v = strings.Replace(v, ".", "", -1)
-			value, err := strconv.ParseInt(v, 10, 64)
-			if err != nil {
-				return accounts, err
-			}
-			netValue = IntDollar(value)
-		case float64:
-			netValue = IntDollar(math.Floor((v * 100) + .5))
+		netValue, err := convertToIntDollar(r.NetAccountValue)
+		if err != nil {
+			return accounts, err
 		}
 		accounts[i].ID = r.AccountID
 		accounts[i].Description = r.Description
@@ -85,6 +114,25 @@ func (client ETradeClient) AccountBalance(accountID int) (raw string, err error)
 	fmt.Println(raw)
 	if err != nil {
 		return raw, err
+	}
+	return
+}
+
+func convertToIntDollar(v interface{}) (i IntDollar, err error) {
+	switch v := v.(type) {
+	default:
+		return i, errors.New("Unexpected type in response for Value")
+	case float32:
+		i = IntDollar(math.Floor((float64(v) * 100) + .5))
+	case string:
+		v = strings.Replace(v, ".", "", -1)
+		value, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			return i, err
+		}
+		i = IntDollar(value)
+	case float64:
+		i = IntDollar(math.Floor((v * 100) + .5))
 	}
 	return
 }
